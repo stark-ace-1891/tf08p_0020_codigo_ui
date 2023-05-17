451e9962f31d9015485b69d195dc8f8821db2e0b
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-17 13:57:01 -0500

    Proyecto inicial

---
+++ android/app/src/main/kotlin/com/example/tf08p_0020_codigo_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tf08p_0020_codigo_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tf08p_0020_codigo_ui/pages/home_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "UI App",
+      home: HomePage(),
+    );
+  }
+}
