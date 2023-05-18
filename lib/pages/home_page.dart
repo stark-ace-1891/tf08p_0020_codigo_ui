6df1072994de413943963622cb0293e9347be56d
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-18 17:43:47 -0500

    Avance header principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
           children: [
             Container(
               padding: EdgeInsets.symmetric(
-                vertical: 12,
+                vertical: 20,
                 horizontal: 16,
               ),
               // height: 300,
@@ -74,6 +74,51 @@ class HomePage extends StatelessWidget {
                           fontWeight: FontWeight.w600,
                         ),
                       ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Find market place",
+                              hintStyle: GoogleFonts.montserrat(
+                                color: Colors.black38,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              prefix: Icon(
+                                Icons.search,
+                                color: Colors.black38,
+                                size: 20,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 14,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: Color(0xffffb404),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Icon(
+                            Icons.filter_alt_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
